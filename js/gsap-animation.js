a72b06b9a7629771b1f7578df79fdc29ab6a3326
Alexandru Cotin <alexandrucotin@Alexandrus-MBP.lan>
2020-03-29 13:01:15 +0200

    more animation

--- js/gsap-animation.js
+++ js/gsap-animation.js
@@ -1,2 +1,5 @@
 gsap.from('.upper__title', {opacity: 0 , duration: 1.5, y:30, delay: 1.2});
-gsap.from('.background', {scale: 1.5 , duration: 1})
+gsap.from('.background', {opacity: 0 , duration: 1.5, z:30});
+gsap.from('.first', {scale: 1.5 , duration: 1})
+gsap.from('.second', {scale: 1 , duration: 1, delay: 1.2})
+gsap.from('.third', {scale: 2 , duration: 1, delay: 1.5})
