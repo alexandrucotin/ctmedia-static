abe8f892f016c2e9bbe9b088ac60455b2bd9ca6c
Alexandru Cotin <alexandrucotin@Alexandrus-MBP.lan>
2020-03-29 15:26:18 +0200

    made menu work

--- js/gsap-animation.js
+++ js/gsap-animation.js
@@ -1,5 +1,7 @@
-gsap.from('.upper__title', {opacity: 0 , duration: 1.5, y:30, delay: 1.2});
-gsap.from('.background', {opacity: 0 , duration: 1.5, z:30});
+gsap.from('.title', {opacity: 0 , duration: 1, y:30});
+gsap.from('.background', {opacity: 0 , duration: 1.5});
 gsap.from('.first', {scale: 1.5 , duration: 1})
-gsap.from('.second', {scale: 1 , duration: 1, delay: 1.2})
-gsap.from('.third', {scale: 2 , duration: 1, delay: 1.5})
+gsap.from('.second', {scale: 1 , duration: 1, })
+gsap.from('.third', {scale: 2 , duration: 1, })
+
+gsap.from('.nineth', {rotation: 360, duration: 7, ease: "elastic", repeat: -1})
