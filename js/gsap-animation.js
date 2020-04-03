4728c9ad2e03455afb656417b0ad2182ad81e2e4
Alexandru Cotin <a.cotin96@gmail.com>
2020-04-03 16:57:37 +0200

    added bagel store

--- js/gsap-animation.js
+++
@@ -1,7 +0,0 @@
-gsap.from('.title', {opacity: 0 , duration: 1, y:30});
-gsap.from('.background', {opacity: 0 , duration: 1.5});
-gsap.from('.first', {scale: 1.5 , duration: 1})
-gsap.from('.second', {scale: 1 , duration: 1, })
-gsap.from('.third', {scale: 2 , duration: 1, })
-
-gsap.from('.nineth', {rotation: 360, duration: 7, ease: "elastic", repeat: -1})
--- js/main.js
+++ js/main.js
@@ -1,42 +1,57 @@
 // Function to add and remove the page transition screen
 function pageTransition() {
+  var tl = gsap.timeline();
+  tl.set(".loading-screen", { transformOrigin: "bottom left" });
+  tl.to(".loading-screen", { duration: 0.5, scaleY: 1 });
+  tl.to(".loading-screen", {
+    duration: 0.5,
+    scaleY: 0,
+    skewX: 0,
+    transformOrigin: "top left",
+    ease: "power1.out",
+    delay: 1
+  });
+  tl.from(".background", { opacity: 0, duration: 1.5 });
+  tl.from(".first", { scale: 1.5, duration: 1 });
+  tl.from(".second", { scale: 1, duration: 1 });
+  tl.from(".third", { scale: 1, duration: 1 });
 
-    var tl = gsap.timeline();
-    tl.set('.loading-screen', { transformOrigin: "bottom left"});
-    tl.to('.loading-screen', { duration: .5, scaleY: 1});
-    tl.to('.loading-screen', { duration: .5, scaleY: 0, skewX: 0, transformOrigin: "top left", ease: "power1.out", delay: 1 });
-  }
-  
-  // Function to animate the content of each page
-  function contentAnimation() {
-  
-    var tl = gsap.timeline();
-    tl.from('.is-animated', { duration: .5, translateY: 10, opacity: 0, stagger: 0.4 });
-    tl.from('.main-navigation', { duration: .5, translateY: -10, opacity: 0});
-  
-    $('.green-heading-bg').addClass('show');
-  
-  }
-  
-  $(function() {
-  
-    barba.init({
-  
-      sync: true,
-  
-      transitions: [{
-  
+  gsap.from(".nineth", {
+    rotation: 360,
+    duration: 7,
+    ease: "elastic",
+    repeat: -1
+  });
+}
+
+// Function to animate the content of each page
+function contentAnimation() {
+  var tl = gsap.timeline();
+  tl.from(".is-animated", {
+    duration: 0.5,
+    translateY: 10,
+    opacity: 0,
+    stagger: 0.4
+  });
+  tl.from(".main-navigation", { duration: 0.5, translateY: -10, opacity: 0 });
+
+  $(".green-heading-bg").addClass("show");
+}
+
+$(function() {
+  barba.init({
+    sync: true,
+
+    transitions: [
+      {
         async leave(data) {
-          
           const done = this.async();
-          
+
           pageTransition();
           await delay(1000);
           done();
-  
         }
-  
-      }]
-    });
-  
+      }
+    ]
   });
+});
